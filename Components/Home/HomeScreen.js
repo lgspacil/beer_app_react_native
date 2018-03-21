6c324e509cce4de5d7c9a95b17d33249e0e5ee05
Lucas Spacil <lucasspacil@Lucass-MacBook-Air.local>
2018-03-21 09:53:52 -0700

    inital commit

---
+++ Components/Home/HomeScreen.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {
+    View, 
+    Text,
+    StyleSheet,
+    Image
+} from "react-native"
+import {Button} from 'native-base';
+var backgroundImage = require('../../assets/HomeScreen/home_screen_background.jpg');
+
+class HomeScreen extends Component {
+
+    //get rid of the header bar for the home screen
+    static navigationOptions = {
+        header: null
+    }
+
+    render(){
+        return (
+            <View style={styles.homeScreenView}>
+                <View style={{position:'absolute', top:0, left:0, height:'100%', width:'100%'}}>
+                    <Image source={backgroundImage} style={{flex:1, height:null, width:null}} />
+                </View>
+                <Button 
+                block = {true}
+                onPress={ () => this.props.navigation.navigate("SearchTabNavigator")}
+                >
+                    <Text style={{color: 'white'}}>Search Beers</Text>
+                </Button>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    homeScreenView: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    }
+})
+
+export default HomeScreen;
---
+++ Components/Search/SearchTabNavigator.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+    View, 
+    Text,
+    StyleSheet
+} from "react-native"
+
+class SearchTabNavigator extends Component {
+    render(){
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+export default SearchTabNavigator;
